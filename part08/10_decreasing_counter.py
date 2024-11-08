d2d9a892d9559771517092a66ffbf10f9cb439e7
Justin David <justin.david@chalhoub.org>
2024-11-08 15:30:18 +0400

    comment part 8, 10

--- part08/10_decreasing_counter.py
+++ part08/10_decreasing_counter.py
@@ -7,6 +7,8 @@ class DecreasingCounter:
     def print_value(self):
         print("value:", self.value)
 
+    # Write the rest of the methods here!
+
     def decrease(self):
         if self.value == 0:
             pass
@@ -18,8 +20,6 @@ class DecreasingCounter:
     
     def reset_original_value(self):
         self.value = self.initial_value
-
-    # Write the rest of the methods here!
 
 if __name__ == "__main__":
     counter = DecreasingCounter(1)
