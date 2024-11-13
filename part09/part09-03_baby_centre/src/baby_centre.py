4ce9669d00cb9e5f2b93ab511d472fe0ee200a9d
Justin David <justindavid2000@gmail.com>
2024-11-13 12:07:41 +0400

    part 9, 3

--- part09/part09-03_baby_centre/src/baby_centre.py
+++ part09/part09-03_baby_centre/src/baby_centre.py
@@ -1,4 +1,3 @@
-
 # WRITE YOUR SOLUTION HERE:
 #Note! Do not change the class Person!
 
@@ -14,5 +13,44 @@ class BabyCentre:
         self.number_of_weigh_ins = 0
 
     def weigh(self, person: Person):
+        # Count the number of times this function has been called
+        self.number_of_weigh_ins += 1
         # return the weight of the person passed as an argument
-        return -1
+        return person.weight
+    
+    def feed(self, person: Person):
+        # adds 1 to the weight of the person
+        person.weight += 1
+
+    def weigh_ins(self):
+        return self.number_of_weigh_ins
+    
+if __name__ == "__main__":
+    baby_centre = BabyCentre()
+
+    eric = Person("Eric", 1, 110, 7)
+    peter = Person("Peter", 33, 176, 85)
+
+    print(f"{eric.name} weighs {baby_centre.weigh(eric)} kg")
+    print(f"{peter.name} weighs {baby_centre.weigh(peter)} kg")
+
+    baby_centre.feed(eric)
+    baby_centre.feed(eric)
+    baby_centre.feed(eric)
+
+    print(f"{eric.name} weighs {baby_centre.weigh(eric)} kg")
+    print(f"{peter.name} weighs {baby_centre.weigh(peter)} kg")
+
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
