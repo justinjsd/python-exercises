06b3016a586da2c3dc63479a70dc089841a3e733
Justin David <justindavid2000@gmail.com>
2024-11-20 14:57:27 +0400

    part 9, 10

--- part09/part09-10_recording/src/recording.py
+++ part09/part09-10_recording/src/recording.py
@@ -1,1 +1,24 @@
 # WRITE YOUR SOLUTION HERE:
+
+class Recording:
+    def __init__(self, length):
+        if length < 0:
+            raise ValueError('Length of recording cannot be below 0.')
+        self.__length = length
+        
+    @property
+    def length(self):
+        return self.__length
+    
+    @length.setter
+    def length(self, length):
+        if length >= 0:
+            self.__length = length
+        else:
+            raise ValueError("The amount must not be below zero")
+        
+if __name__ == "__main__":
+    the_wall = Recording(-1)
+    print(the_wall.length)
+    the_wall.length = 44
+    print(the_wall.length)
