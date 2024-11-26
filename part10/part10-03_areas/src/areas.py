05618d68f98a77ec2b7f1f64e0cb1086c200af95
Justin David <justindavid2000@gmail.com>
2024-11-26 23:41:22 +0400

    part 10, 3

--- part10/part10-03_areas/src/areas.py
+++ part10/part10-03_areas/src/areas.py
@@ -9,3 +9,19 @@ class Rectangle:
 
     def area(self):
         return self.width * self.height
+    
+class Square(Rectangle):
+    def __init__(self, width):
+        super().__init__(width, width)
+
+    def __str__(self):
+        return f"square {self.width}x{self.width}"
+    
+if __name__ == "__main__":
+    rectangle = Rectangle(2, 3)
+    print(rectangle)
+    print("area:", rectangle.area())
+
+    square = Square(4)
+    print(square)
+    print("area:", square.area())
