14b42cae10b9bc2f9fca5840d7113004cd58eaa0
Justin David <justindavid2000@gmail.com>
2024-11-28 21:03:37 +0400

    part 10, 4

--- part10/part10-04_word_game/src/word_game.py
+++ part10/part10-04_word_game/src/word_game.py
@@ -31,3 +31,53 @@ class WordGame():
         print(f"player 1: {self.wins1}")
         print(f"player 2: {self.wins2}")
 
+class LongestWord(WordGame):
+    def __init__(self, rounds: int):
+        super().__init__(rounds)
+
+    def round_winner(self, player1_word: str, player2_word: str):
+        if len(player1_word) > len(player2_word):
+            return 1
+        elif len(player1_word) < len(player2_word): 
+            return 2
+        else:
+            return 0
+
+class MostVowels(WordGame):
+    def __init__(self, rounds: int):
+        super().__init__(rounds)
+
+    def count_vowels(self, word):
+        vowels = "aeiouAEIOU"  # List of vowels (both uppercase and lowercase)
+        count = 0
+        for char in word:
+            if char in vowels:
+                count += 1
+        return count
+
+    def round_winner(self, player1_word: str, player2_word: str):
+        vowels1 = self.count_vowels(player1_word)
+        vowels2 = self.count_vowels(player2_word)
+
+        if vowels1 > vowels2:
+            return 1
+        elif vowels2 > vowels1:
+            return 2
+        else:
+            pass
+
+class RockPaperScissors(WordGame):
+    def __init__(self, rounds: int):
+        super().__init__(rounds)
+
+    def round_winner(self, player1_word: str, player2_word: str):
+        game_list = ["rock", "paper", "scissors"]
+
+        if (player1_word in game_list and player2_word not in game_list) or (player1_word == "rock" and player2_word == "scissors") or (player1_word == "paper" and player2_word == "rock") or (player1_word == "scissors" and player2_word == "paper"):
+            return 1
+        elif (player2_word in game_list and player1_word not in game_list) or (player2_word == "rock" and player1_word == "scissors") or (player2_word == "paper" and player1_word == "rock") or (player2_word == "scissors" and player1_word == "paper"):
+            return 2
+        
+if __name__ == "__main__":
+    p = MostVowels(1)
+    p.play()
