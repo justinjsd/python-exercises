d8b7bfe4b96ba97486179a12fff289af7ba645a0
Justin David <justindavid2000@gmail.com>
2024-11-30 16:12:19 +0400

    part 10, 9

--- part10/part10-09_iterable_shopping_list/src/iterable_shopping_list.py
+++ part10/part10-09_iterable_shopping_list/src/iterable_shopping_list.py
@@ -14,3 +14,34 @@ class ShoppingList:
 
     def number(self, n: int):
         return self.products[n - 1][1]
+    
+    # This is the iterator initialization method
+    # The iteration variable(s) should be initialized here
+    def __iter__(self):
+        self.n = 0
+        # the method returns a reference to the object itself as 
+        # the iterator is implemented within the same class definition
+        return self
+
+    # This method returns the next item within the object
+    # If all items have been traversed, the StopIteration event is raised
+    def __next__(self):
+        if self.n < len(self.products):
+            # Select the current item from the list within the object
+            product = self.products[self.n]
+            # increase the counter (i.e. iteration variable) by one
+            self.n += 1
+            # return the current item
+            return product
+        else:
+            # All books have been traversed
+            raise StopIteration
+
+if __name__ == "__main__":
+    shopping_list = ShoppingList()
+    shopping_list.add("bananas", 10)
+    shopping_list.add("apples", 5)
+    shopping_list.add("pineapple", 1)
+
+    for product in shopping_list:
+        print(f"{product[0]}: {product[1]} units")
