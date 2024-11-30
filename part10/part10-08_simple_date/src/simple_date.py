0a63c16120dbb7e3175a0c2d97c2482aeb77836a
Justin David <justindavid2000@gmail.com>
2024-11-30 15:56:20 +0400

    part 10, 8 rewrite

--- part10/part10-08_simple_date/src/simple_date.py
+++ part10/part10-08_simple_date/src/simple_date.py
@@ -1,40 +1,43 @@
 # WRITE YOUR SOLUTION HERE:
 
 class SimpleDate:
-    def __init__(self, date, month, year):
-        self.date = date 
-        self.month = month
-        self.year = year
-
+    def __init__(self, day: int, month: int, year: int):
+        self.__day = day
+        self.__month = month
+        self.__year = year
+    
     def __str__(self):
-        return f"{self.date}.{self.month}.{self.year}"
+        return f'{self.__day}.{self.__month}.{self.__year}'
     
-    def __eq__(self, value):
-        return self.__str__() == value.__str__()
-    
-    def __ne__(self, value):
-        return self.__str__() != value.__str__()
-    
-    def __gt__(self, value):
-        if self.year > value.year:
+    def __lt__(self, another: 'SimpleDate'):
+        if self.__year < another.__year:
             return True
-        elif self.year == value.year and self.month > value.month:
+        elif self.__year == another.__year:
+            if self.__month < another.__month:
+                return True
+            elif self.__month == another.__month:
+                if self.__day < another.__day:
+                    return True
+
+        return False
+
+    def __gt__(self, another: 'SimpleDate'):
+        if self.__eq__(another) == False and self.__lt__(another) == False:
             return True
-        elif self.month == value.month and self.date > value.date:
+        return False
+
+    def __eq__(self, another: 'SimpleDate'):
+        if self.__day == another.__day and \
+            self.__month == another.__month and \
+                self.__year == another.__year:
+                return True
+        return False
+
+    def __ne__(self, another: 'SimpleDate'):
+        if self.__eq__(another) == False:
             return True
-        else: 
-            return False
-        
-    def __lt__(self, value):
-        if self.year < value.year:
-            return True
-        elif self.year == value.year and self.month < value.month:
-            return True
-        elif self.month == value.month and self.date < value.date:
-            return True
-        else: 
-            return False
-        
+        return False
+
     def __convert_curr_date_to_days(self):
         return ((self.__year-1)*12*30) + ((self.__month-1)*30) + self.__day
 
@@ -45,12 +48,12 @@ class SimpleDate:
         month = ((days//30)%12)+1
         day = days%30
         return SimpleDate(day, month, year)
-        
+
     def __sub__(self, another: 'SimpleDate'):
         curr_days = self.__convert_curr_date_to_days()
         another_days = another.__convert_curr_date_to_days()
         return abs(curr_days - another_days)
-    
+
 if __name__ == "__main__":
     d1 = SimpleDate(4, 10, 2020)
     d2 = SimpleDate(28, 12, 1985)
