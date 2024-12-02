708964420baee8e10fa60251225b04e9a76f0f38
Justin David <justindavid2000@gmail.com>
2024-12-02 19:36:49 +0400

    part 10, 10

--- part10/part10-10_phone_book_v1/src/phone_book_v1.py
+++ part10/part10-10_phone_book_v1/src/phone_book_v1.py
@@ -5,7 +5,6 @@ class PhoneBook:
 
     def add_number(self, name: str, number: str):
         if not name in self.__persons:
-            # add a new dictionary entry with an empty list for the numbers
             self.__persons[name] = []
 
         self.__persons[name].append(number)
@@ -15,6 +14,12 @@ class PhoneBook:
             return None
         return self.__persons[name]
 
+    def get_name(self, number: str):
+        for person, phone_numbers in self.__persons.items():
+            for pn in phone_numbers:
+                if pn == number:
+                    return person
+        return None
 
     def all_entries(self):
         return self.__persons
@@ -53,6 +58,7 @@ class PhoneBookApplication:
         print("0 exit")
         print("1 add entry")
         print("2 search")
+        print("3 search by number")
 
     def add_entry(self):
         name = input("name: ")
@@ -71,6 +77,13 @@ class PhoneBookApplication:
     def exit(self):
         self.__filehandler.save_file(self.__phonebook.all_entries())
 
+    def search_by_number(self):
+        number = input("number: ")
+        name = self.__phonebook.get_name(number)
+        if name == None:
+            print("unknown number")
+        else:
+            print(name)
 
     def execute(self):
         self.help()
@@ -84,9 +97,10 @@ class PhoneBookApplication:
                 self.add_entry()
             elif command == "2":
                 self.search()
+            elif command == "3":
+                self.search_by_number()
             else:
                 self.help()
 
-# when you run the tests, nothing apart from these two lines should be placed in the main function, outside any class definitions 
 application = PhoneBookApplication()
 application.execute()
