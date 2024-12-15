56bf7d66fa1a691966ad9e7b19fc39957b7340e5
Justin David <justindavid2000@gmail.com>
2024-12-15 16:24:37 +0400

    part 11, 8

--- part11/part11-08_filter_forbidden/src/filter_forbidden.py
+++ part11/part11-08_filter_forbidden/src/filter_forbidden.py
@@ -1,1 +1,10 @@
 # WRITE YOUR SOLUTION HERE:
+
+def filter_forbidden(string: str, forbidden: str):
+    filtered_string = "".join([character for character in string if character not in forbidden])
+    return filtered_string
+
+if __name__ == "__main__":
+    sentence = "Once! upon, a time: there was a python!??!?!"
+    filtered = filter_forbidden(sentence, "!?:,.")
+    print(filtered)
