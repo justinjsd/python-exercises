284ada812d8fd1d7162af19cb3905b72d9a208ab
Justin David <justindavid2000@gmail.com>
2024-12-17 15:56:06 +0400

    part 11, 15

--- part11/part11-15_balanced_brackets/src/balanced_brackets.py
+++ part11/part11-15_balanced_brackets/src/balanced_brackets.py
@@ -1,9 +1,13 @@
-
 def balanced_brackets(my_string: str):
+    my_string = ''.join([char for char in my_string if char in "()[]"])
     if len(my_string) == 0:
         return True
-    if not (my_string[0] == '(' and my_string[-1] == ')'):
+    if not ((my_string[0] == '(' and my_string[-1] == ')') or (my_string[0] == '[' and my_string[-1] == ']')) :
         return False
 
     # remove first and last character
     return balanced_brackets(my_string[1:-1])
+
+if __name__ == "__main__":
+    ok = balanced_brackets("(Hello)")
+    print(ok)   
