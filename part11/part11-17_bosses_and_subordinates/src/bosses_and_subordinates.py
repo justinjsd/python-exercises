41a5f6eb2eaa443418523537a15079a8aa76a85e
Justin David <justindavid2000@gmail.com>
2024-12-19 10:31:39 +0400

    part 11, 17

--- part11/part11-17_bosses_and_subordinates/src/bosses_and_subordinates.py
+++ part11/part11-17_bosses_and_subordinates/src/bosses_and_subordinates.py
@@ -6,3 +6,26 @@ class Employee:
 
     def add_subordinate(self, employee: 'Employee'):
         self.subordinates.append(employee)
+
+def count_subordinates(employee: Employee):
+    sub = 0
+    for emp in employee.subordinates:
+        sub += 1
+        sub += count_subordinates(emp)
+    return sub
+
+if __name__ == "__main__":
+    t1 = Employee("Sally")
+    t2 = Employee("Eric")
+    t3 = Employee("Matthew")
+    t4 = Employee("Emily")
+    t5 = Employee("Adele")
+    t6 = Employee("Claire")
+    t1.add_subordinate(t4)
+    t1.add_subordinate(t6)
+    t4.add_subordinate(t2)
+    t4.add_subordinate(t3)
+    t4.add_subordinate(t5)
+    print(count_subordinates(t1))
+    print(count_subordinates(t4))
+    print(count_subordinates(t5))
