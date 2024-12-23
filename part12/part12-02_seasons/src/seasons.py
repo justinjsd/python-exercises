5e79cb5df1c67ad14f9eda53a2d2a365549b0d4d
Justin David <justindavid2000@gmail.com>
2024-12-23 20:36:53 +0400

    part 12, 02

--- part12/part12-02_seasons/src/seasons.py
+++ part12/part12-02_seasons/src/seasons.py
@@ -1,1 +1,13 @@
-# Write your solution here:
+# Write your solution here
+
+def seasons(shows: dict) -> int:
+    return shows["seasons"]
+
+def sort_by_seasons(items: list) -> list:
+    return sorted(items, key=seasons)
+
+if __name__ == "__main__":
+    shows = [{ "name": "Dexter", "rating" : 8.6, "seasons":9 }, { "name": "Friends", "rating" : 8.9, "seasons":10 },  { "name": "Simpsons", "rating" : 8.7, "seasons":32 }  ]
+
+    for show in sort_by_seasons(shows):
+        print(f"{show['name']} {show['seasons']} seasons")
