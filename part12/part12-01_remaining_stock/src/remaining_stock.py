42a2cfd88c3923e0411db3c51ecdd1944cb1626d
Justin David <justindavid2000@gmail.com>
2024-12-23 20:30:09 +0400

    part 12, 01

--- part12/part12-01_remaining_stock/src/remaining_stock.py
+++ part12/part12-01_remaining_stock/src/remaining_stock.py
@@ -1,1 +1,13 @@
 # Write your solution here:
+
+def remaining_stock(products: list):
+    return products[2]
+
+def sort_by_remaining_stock(products: list):
+    return sorted(products, key=remaining_stock)
+
+if __name__ == "__main__":
+    products = [("banana", 5.95, 12), ("apple", 3.95, 3), ("orange", 4.50, 2), ("watermelon", 4.95, 22)]
+
+    for product in sort_by_remaining_stock(products):
+        print(f"{product[0]} {product[2]} pcs")
