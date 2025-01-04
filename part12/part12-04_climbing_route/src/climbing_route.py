3aef577be29ed990baeadc96507aa081a3ceba65
Justin David <justindavid2000@gmail.com>
2025-01-04 19:51:22 +0400

    part 12, 4 - 2

--- part12/part12-04_climbing_route/src/climbing_route.py
+++ part12/part12-04_climbing_route/src/climbing_route.py
@@ -12,16 +12,31 @@ class ClimbingRoute:
 def by_length(route: ClimbingRoute):
     return route.length
 
+def by_difficulty(route: ClimbingRoute):
+    return (route.grade, route.length)
+
 def sort_by_length(routes: list):
     return sorted(routes, key = by_length, reverse=True)
 
+def sort_by_difficulty(routes: list):
+    return sorted(routes, key = by_difficulty, reverse=True)
+
 if __name__ == "__main__":
+    # r1 = ClimbingRoute("Edge", 38, "6A+")
+    # r2 = ClimbingRoute("Smooth operator", 11, "7A")
+    # r3 = ClimbingRoute("Synchro", 14, "8C+")
+    # r4 = ClimbingRoute("Small steps", 12, "6A+")
+
+    # routes = [r1, r2, r3, r4]
+
+    # for route in sort_by_length(routes):
+    #     print(route)
+
     r1 = ClimbingRoute("Edge", 38, "6A+")
     r2 = ClimbingRoute("Smooth operator", 11, "7A")
     r3 = ClimbingRoute("Synchro", 14, "8C+")
     r4 = ClimbingRoute("Small steps", 12, "6A+")
 
     routes = [r1, r2, r3, r4]
-
-    for route in sort_by_length(routes):
+    for route in sort_by_difficulty(routes):
         print(route)
