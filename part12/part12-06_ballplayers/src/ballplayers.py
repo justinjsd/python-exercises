2b7e1c8d3bbe5960fcdba3720c6e3dec928cd4a2
Justin David <justindavid2000@gmail.com>
2025-01-05 16:40:04 +0400

    part 12, 6 - 1

--- part12/part12-06_ballplayers/src/ballplayers.py
+++ part12/part12-06_ballplayers/src/ballplayers.py
@@ -9,6 +9,9 @@ class BallPlayer:
     def __str__(self):
         return (f'BallPlayer(name={self.name}, number={self.number}, '
             f'goals={self.goals}, passes={self.passes}, minutes={self.minutes})')
-
+    
+def most_goals(ballplayers: list):
+    ballplayer = max(ballplayers, key=lambda bp: bp.goals)
+    return ballplayer.name
 
 # Write your solution here
