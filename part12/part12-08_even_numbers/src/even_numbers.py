a0579cff5539f079be5a0b7ff65839e7998cdb02
Justin David <justindavid2000@gmail.com>
2025-01-06 16:57:45 +0400

    part 12, 8

--- part12/part12-08_even_numbers/src/even_numbers.py
+++ part12/part12-08_even_numbers/src/even_numbers.py
@@ -1,1 +1,11 @@
 # Write your solution here
+
+def even_numbers(num: int, end: int):
+    while num <= end:
+        yield num
+        num += 2
+
+if __name__ == "__main__":
+    numbers = even_numbers(2, 10)
+    for number in numbers:
+        print(number)
