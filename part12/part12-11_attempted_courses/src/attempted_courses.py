233cafd35e8f59da86709b528496ff3ba17a51bc
Justin David <justindavid2000@gmail.com>
2025-01-09 15:53:42 +0400

    part 12, 11

--- part12/part12-11_attempted_courses/src/attempted_courses.py
+++ part12/part12-11_attempted_courses/src/attempted_courses.py
@@ -12,10 +12,16 @@ class CourseAttempt:
 def names_of_students(attempts: list):
     return list(map(lambda t: t.student_name,attempts))
 
+def course_names(attempts: list):
+    return set(list(map(lambda t: t.course_name,attempts)))
+
 if __name__ == "__main__":
     s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
     s2 = CourseAttempt("Olivia C. Objective", "Introduction to Programming", 5)
     s3 = CourseAttempt("Peter Python", "Advanced Course in Programming", 2)
 
     for name in names_of_students([s1, s2, s3]):
+        print(name)
+
+    for name in course_names([s1, s2, s3]):
         print(name)
