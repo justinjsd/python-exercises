526104ab78837574d9f01ff4d801188f243b8253
Justin David <justindavid2000@gmail.com>
2025-01-12 18:48:07 +0400

    part 12, 14 - part 1

--- part12/part12-14_regular_expressions/src/regular_expressions.py
+++ part12/part12-14_regular_expressions/src/regular_expressions.py
@@ -1,2 +1,13 @@
 # Write your solution here
 import re
+
+def is_dotw(my_string: str):
+    if re.search("Mon|Tue|Wed|Thu|Fri|Sat|Sun", my_string):
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    print(is_dotw("Mon"))
+    print(is_dotw("Fri"))
+    print(is_dotw("Tui"))
