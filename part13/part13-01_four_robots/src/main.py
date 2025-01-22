d44cfc9d49a3db78d3f79be8c8f04a9bde9e1d54
Justin David <justindavid2000@gmail.com>
2025-01-22 16:04:29 +0400

    part 13, 01

--- part13/part13-01_four_robots/src/main.py
+++ part13/part13-01_four_robots/src/main.py
@@ -1,4 +1,3 @@
-
 # The exercises in this part of the course have no automated tests, as the results as visually verified.
 # The tests grant points automatically as you submit your solution to the server, no matter what your implementation.
 # Only submit your solution when you are ready, and your solution matches the exercise description.
@@ -7,3 +6,28 @@
 
 # WRITE YOUR SOLUTION HERE:
 
+import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode((640, 480))
+
+width_window = 640
+height_window = 480
+
+robot = pygame.image.load("robot.png")
+
+width = robot.get_width()
+height = robot.get_height()
+
+window.fill((0, 0, 0))
+window.blit(robot, (0, 0))
+window.blit(robot, (width_window-width, 0))
+window.blit(robot, (0, height_window-height))
+window.blit(robot, (width_window-width, height_window-height))
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
