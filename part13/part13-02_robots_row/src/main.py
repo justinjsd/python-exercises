5b2146194c7b065b2a74cb810f1c10dc0ec72b02
Justin David <justindavid2000@gmail.com>
2025-01-22 16:08:29 +0400

    part 13, 02

--- part13/part13-02_robots_row/src/main.py
+++ part13/part13-02_robots_row/src/main.py
@@ -1,1 +1,21 @@
 # WRITE YOUR SOLUTION HERE:
+
+import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+
+width = robot.get_width()
+
+window.fill((0, 0, 0))
+for i in range(0,10):
+    window.blit(robot,(75+width*i,200))
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
