be3cd0b59a588762b5c2ee99549388f2b79b6eed
Justin David <justindavid2000@gmail.com>
2025-01-23 11:44:14 +0400

    part 13, 04

--- part13/part13-04_random_robots/src/main.py
+++ part13/part13-04_random_robots/src/main.py
@@ -1,1 +1,29 @@
 # WRITE YOUR SOLUTION HERE:
+from random import randint
+import pygame
+
+pygame.init()
+
+window_width = 640
+window_height = 480
+
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+
+width = robot.get_width()
+height = robot.get_height()
+
+window.fill((0, 0, 0))
+
+for i in range(1000):
+    x = randint(0, window_width-width)
+    y = randint(0, window_height-height)
+    window.blit(robot, (x, y))
+
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
