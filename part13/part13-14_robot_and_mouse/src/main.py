c6b9378173cbc0831fcd8cd70fcddcc96bfcdb14
Justin David <justindavid2000@gmail.com>
2025-02-01 13:35:21 +0400

    part 13, 14 - fix

--- part13/part13-14_robot_and_mouse/src/main.py
+++ part13/part13-14_robot_and_mouse/src/main.py
@@ -1,6 +1,5 @@
 # WRITE YOUR SOLUTION HERE:
 import pygame
-import random
 
 pygame.init()
 
@@ -12,24 +11,18 @@ robot = pygame.image.load("robot.png")
 width = robot.get_width()
 height = robot.get_height()
 
-robot_x = random.randint(0, window_width - width)
-robot_y = random.randint(0, window_height - height)
+robot_x = 0
+robot_y = 0
 
 clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
-
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Left-click (button 1)
-            mouse_x, mouse_y = event.pos  # Get mouse click position
+        if event.type == pygame.MOUSEMOTION:
+            robot_x = event.pos[0]-width/2
+            robot_y = event.pos[1]-height/2
             
-            # Check if the click is inside the robot's area
-            if robot_x <= mouse_x <= robot_x + width and robot_y <= mouse_y <= robot_y + height:
-                robot_x = random.randint(0, window_width)
-                robot_y = random.randint(0, window_height)
-
         if event.type == pygame.QUIT:
-            pygame.quit()
             exit(0)
 
     window.fill((0, 0, 0))
